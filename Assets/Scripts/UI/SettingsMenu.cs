83b07c265d141852520f2a1a3c926ed067bdaf07
Tihana Krajtmajer <71774763+tkrajtmajer@users.noreply.github.com>
2026-08-14 14:24:40 +0200

    Separate SFX and music volume

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public UnityEngine.UI.Slider volumeSlider;
+    public UnityEngine.UI.Slider volumeMusicSlider;
+    public UnityEngine.UI.Slider volumeSFXSlider;
 
-    public void SetVolume(float volume) {
-        Debug.Log("volume " + volume);
-        volumeSlider.value = volume;
+    public void SetVolumeMusic(float volume) {
+        // Debug.Log("volume " + volume);
+        volumeMusicSlider.value = volume;
         MusicManager.Instance.audioSource.volume = volume;
+    }
+
+    public void SetVolumeSFX(float volume) {
+        // Debug.Log("volume " + volume);
+        volumeSFXSlider.value = volume;
+        SFXManager.Instance.audioSource.volume = volume;
     }
 }
